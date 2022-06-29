dabed8910c14acf53fdc8e171602661a472de2fb
Harrison Sabean <hsabes@gmail.com>
2022-06-29 16:07:30 -0700

    commit tree 21343b6c5ff406f6320004c7a5e0ad59dbc0eac7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+
+    attr_accessor :total, :discount, :price, :add_item, :title, :items, :quantity
+    
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        @title = title
+        @price = price
+        @quantity = quantity
+        @total += (price * quantity)
+        @items = @items.fill(@title, @items.length, quantity)
+        @void_last_transaction = price
+    end
+
+    def apply_discount
+        if (@discount > 0)
+            @total = (@price - (@price * (@discount.to_f / 100.00))).to_i
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @void_last_transaction
+        @total = @total - (@void_last_transaction * @quantity)
+    end
+
+end
+
+harry = CashRegister.new
+binding.pry
